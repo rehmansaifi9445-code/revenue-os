4d72c13e67d3e451320ca85bf6e53361232b9d08
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-21 11:06:55 +0530

    Create user.module.ts

---
+++ backend/src/modules/user/dto/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
+
+import { PrismaModule } from '../../database/prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [UserController],
+  providers: [UserService, UserRepository],
+  exports: [UserService],
+})
+export class UserModule {}
