73ccaabb66ae9dcfa02fece443f4e5c0b0663057
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-24 08:00:52 +0530

    Create update-reminder.dto.ts

---
+++ backend/src/modules/reminder/dto/update-reminder.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateReminderDto } from './create-reminder.dto';
+
+export class UpdateReminderDto extends PartialType(
+  CreateReminderDto,
+) {}
