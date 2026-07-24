32cf18f5969af43dfbd857c37cd54bc71538a202
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-24 21:26:12 +0530

    Create update-configuration.dto.ts

---
+++ backend/src/modules/configuration/dto/update-configuration.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateConfigurationDto } from './create-configuration.dto';
+
+export class UpdateConfigurationDto extends PartialType(
+  CreateConfigurationDto,
+) {}
