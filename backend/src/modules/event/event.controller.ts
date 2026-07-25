bfd7f90181adcb971f369e3a88c30860c2331484
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-25 10:33:11 +0530

    Create event.controller.ts

---
+++ backend/src/modules/event/event.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
+
+import { EventService } from './event.service';
+
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { EventQueryDto } from './dto/event-query.dto';
+
+@Controller('events')
+export class EventController {
+  constructor(
+    private readonly eventService: EventService,
+  ) {}
+
+  @Post()
+  create(@Body() createEventDto: CreateEventDto) {
+    return this.eventService.create(createEventDto);
+  }
+
+  @Get()
+  findAll(@Query() query: EventQueryDto) {
+    return this.eventService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateEventDto: UpdateEventDto,
+  ) {
+    return this.eventService.update(id, updateEventDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.eventService.remove(id);
+  }
+}
