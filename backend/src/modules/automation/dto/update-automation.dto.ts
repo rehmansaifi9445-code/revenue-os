b088c9566df1d03aab1e71fcf59013e6e20f0d5b
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-27 08:48:03 +0530

    Create update-automation.dto.ts

---
+++ backend/src/modules/automation/dto/update-automation.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateAutomationDto } from './create-automation.dto';
+
+export class UpdateAutomationDto extends PartialType(
+  CreateAutomationDto,
+) {}
