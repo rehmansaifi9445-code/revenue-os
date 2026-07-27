a4271c1635f6555c6d6b73be0168b25885cd00c0
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-27 13:50:15 +0530

    Create update-scheduler.dto.ts

---
+++ backend/src/modules/scheduler/dto/update-scheduler.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateSchedulerDto } from './create-scheduler.dto';
+
+export class UpdateSchedulerDto extends PartialType(
+  CreateSchedulerDto,
+) {}
