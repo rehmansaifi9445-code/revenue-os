75f75a9678c38ae9ff6bc8b4020986509614a7a3
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-31 18:18:56 +0530

    Create update-security.dto.ts

---
+++ backend/src/modules/security/dto/update-security.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateSecurityDto } from './create-security.dto';
+
+export class UpdateSecurityDto extends PartialType(
+  CreateSecurityDto,
+) {}
