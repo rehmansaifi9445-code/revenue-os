9edc30ec06ceb413585c5eaa43fe4e3417af9881
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-31 17:56:13 +0530

    Create update-monitoring.dto.ts

---
+++ backend/src/modules/monitoring/dto/update-monitoring.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateMonitoringDto } from './create-monitoring.dto';
+
+export class UpdateMonitoringDto extends PartialType(
+  CreateMonitoringDto,
+) {}
