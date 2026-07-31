b34fe59db4d2705241ea69dc50f5aab6cfd625dc
rehmansaifi9445-code <rehmansaifi9445@gmail.com>
2026-07-31 13:40:02 +0530

    Create update-analytics.dto.ts

---
+++ backend/src/modules/analytics/dto/update-analytics.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateAnalyticsDto } from './create-analytics.dto';
+
+export class UpdateAnalyticsDto extends PartialType(
+  CreateAnalyticsDto,
+) {}
